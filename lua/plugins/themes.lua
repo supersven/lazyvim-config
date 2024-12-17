88bc5a2dfa01c80b32313c5e52d0c21f4be7bc55
Sven Tennie <sven.tennie@wire.com>
2024-12-17 14:29:06 +0100

    Add catppuccin theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,3 +1,9 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    }
+  }
 }
