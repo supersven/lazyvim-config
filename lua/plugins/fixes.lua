5a69947a7073738595d159086d9ef76dd3cced93
Sven Tennie <sven.tennie@gmail.com>
2024-12-21 09:47:03 +0100

    Fix blink.cmp (by downgrading)
    See https://github.com/Saghen/blink.cmp/issues/676

---
+++ lua/plugins/fixes.lua
@@ -0,0 +1,9 @@
+-- Temporary overrides to fix things
+-- TODO: Check regularly if these are still required.
+return {
+
+  -- See https://github.com/Saghen/blink.cmp/issues/676
+  {
+    "blink.cmp",
+    version = "v0.7.6"
+  } }
