26350ecd214e8997834f72433426e06705d912e8
Sven Tennie <sven.tennie@wire.com>
2025-02-19 16:04:20 +0100

    Fix import position of extra plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,9 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.haskell" },
+     -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
