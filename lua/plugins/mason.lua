c64daa702faecfe554296febc13e58f09a01d624
Sven Tennie <sven.tennie@wire.com>
2026-01-24 17:38:32 +0100

    Format mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,5 @@
 -- Turn off mason. It does make sense in a Nix context.
 return {
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
- { "williamboman/mason.nvim", enabled = false },
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
+  { "mason-org/mason.nvim",           enabled = false },
 }
