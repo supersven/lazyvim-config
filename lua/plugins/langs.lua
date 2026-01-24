ca340668e7d9fdfa257809f94f9ef0cc0401c657
Sven Tennie <sven.tennie@wire.com>
2026-01-24 17:38:52 +0100

    Fix Haskell setup

--- lua/plugins/langs.lua
+++ lua/plugins/langs.lua
@@ -1,6 +1,29 @@
 return {
   {
     "mrcjkb/haskell-tools.nvim",
-    version = "^4",
+    version = "v6.2.0",
+    lazy = false,
+    init = function()
+      vim.g.haskell_tools = {
+        hls = {
+          auto_attach = true,
+          settings = {
+            haskell = {
+              checkProject = false,
+              plugin = {
+                semanticTokens = {
+                  globalOn = true,
+                },
+                importLens = { -- make import lists fully explicit
+                  codeActionsOn = false,
+                  codeLensOn = false,
+                },
+              },
+              sessionLoading = "multipleComponents"
+            },
+          },
+        },
+      }
+    end,
   }
 }
